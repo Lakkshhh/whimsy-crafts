9269e5827948d6a89643260627fca2f3259d541f
Lakksh Tyagi <lakksh.tyagi22@gmail.com>
2024-06-20 20:41:37 +0530

    PDF order confirmation attachment sent through email functionality added

--- src/main/java/com/eCommerceSite/WhimsyCrafts/services/MailConfig.java
+++ src/main/java/com/eCommerceSite/WhimsyCrafts/services/MailConfig.java
@@ -1,5 +1,6 @@
 package com.eCommerceSite.WhimsyCrafts.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("lakkshtags@gmail.com");
-        mailSender.setPassword("fucs ybbh goaj gzaz");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
